12fd5fd5c0f19fc3d63a05fba524e0dd8385a8ff
steamyowl <102464319+steamedowl@users.noreply.github.com>
2025-01-26 12:58:42 -0800

    Crabs be walkin

--- scripts/class/crab.gd
+++ scripts/class/crab.gd
@@ -11,6 +11,10 @@ extends CharacterBody3D
 @onready var crab_model: Node3D = $CollisionShape3D/CrabModel
 @onready var scuttle_vfx: AudioStreamPlayer = $ScuttleVFX
 
+
+@onready var random_move_pos_target : Vector3 = Vector3.ZERO
+@onready var currently_random_moving : bool = false
+
 var current_fixation = null
 
 func _ready() -> void:
@@ -23,6 +27,9 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	var do_we_want_to_move : bool = false
+	var move_target_pos : Vector3 = Vector3.ZERO
 	# check if an object is within the crab's FOV
 	var objects_within_fov : Array[Node3D] = crab_vision.get_overlapping_bodies()
 	# sort objects by distance from crab
@@ -32,20 +39,43 @@ func _physics_process(delta: float) -> void:
 		current_fixation = closest_object
 		if current_fixation.get_groups().find("hand") >= 0:
 			# crabs will be attracted to the hand that feeds them
-			var direction = current_fixation.position - position
-			if direction:
-				velocity.x = direction.x * speed
-				velocity.z = direction.z * speed
-			
-			var current_ang : float = global_rotation.y
-			look_at(current_fixation.position)
-			var modified_ang : float = global_rotation.y
-			global_rotation.y = lerp_angle(current_ang, modified_ang, delta*2.0)
-		else:
-			velocity = Vector3.ZERO
-			scuttle_vfx.stop()
+			do_we_want_to_move = true
+			move_target_pos = current_fixation.position
 	else:
 		current_fixation = null
+	
+	if !do_we_want_to_move:
+		if !currently_random_moving:#we're chillin, should we start moving?
+			var rand_chance : float =  randf_range(0.0, 1.0) * delta
+			
+			if rand_chance < 0.005:#we're starting a move
+				currently_random_moving = true
+				random_move_pos_target = Vector3(randf_range(-11.0, 11.0), -1.5, randf_range(-5.5, 5.5))
+		if currently_random_moving:
+			move_target_pos = random_move_pos_target
+			do_we_want_to_move = true
+			currently_random_moving = (move_target_pos-global_position).length()>0.55*scale.x
+	else:#if we're fixatin, random move should be on ice
+		currently_random_moving = false
+	
+	#we know if and where we wnat to move, lets go
+	if do_we_want_to_move:
+		var direction = move_target_pos - position
+		var speed_mod : float = 1.0
+		if currently_random_moving: speed_mod = 0.3
+		if direction:
+			velocity.x = direction.x * speed * speed_mod
+			velocity.z = direction.z * speed * speed_mod
+		
+		var look_target : Vector3 = move_target_pos
+		if currently_random_moving:
+			look_target = global_position + (look_target - global_position).rotated(Vector3.UP,deg_to_rad(90))
+		
+		var current_ang : float = global_rotation.y
+		look_at(look_target)
+		var modified_ang : float = global_rotation.y
+		global_rotation.y = lerp_angle(current_ang, modified_ang, delta*2.0)
+	else:
 		velocity = Vector3.ZERO
 		scuttle_vfx.stop()
 		
