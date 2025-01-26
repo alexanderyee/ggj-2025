c1ba68e49917212ba88170b29ad87b070c2eb805
adriannarieke <adriannarieke@users.noreply.github.com>
2025-01-25 23:17:35 -0800

    WIP, almost done with implementing the need emote bubble component.

--- scripts/class/needemotes.gd
+++ scripts/class/needemotes.gd
@@ -1,14 +1,28 @@
 extends Needs
 
+@export var foodXCoord : int = 0
+@export var shelterXCoord : int = 20
+@export var comfortXCoord : int = 40
+@export var communityXCoord : int = 60
+
+## emoteID should go from 0 - 8. 0 is the 'nothing' emote. 
+@export var emoteID : int = 0
+
+@export var speechSprite : Sprite3D
+@export var foodSprite : Sprite3D
+@export var shelterSprite : Sprite3D
+@export var comfortSprite : Sprite3D
+@export var communitySprite : Sprite3D
+
 var foodTimer : float
 var shelterTimer : float
 var comfortTimer : float
 var communityTimer : float
 
-var foodXCoord : int = 0
-var shelterXCoord : int = 20
-var comfortXCoord : int = 40
-var communityXCoord : int = 60
+var foodEmoteTimer : float
+var shelterEmoteTimer : float
+var comfortEmoteTimer : float
+var communityEmoteTimer : float
 
 var prevFood : int
 var prevShelter : int
@@ -21,6 +35,12 @@ func _ready() -> void:
 	prevShelter = shelter
 	prevComfort = comfort
 	prevCommunity = community
+	
+	speechSprite.transparency = 1
+	foodSprite.transparency = 1
+	shelterSprite.transparency = 1
+	comfortSprite.transparency = 1
+	communitySprite.transparency = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -30,9 +50,49 @@ func _process(delta: float) -> void:
 	checkneed(2)
 	checkneed(3)
 
-##  0-3
-func checkneed(stat_id: int): 
+func emote(stat_id):
+	findsprite(stat_id)
+	startspritetimer(stat_id)
+	setcoords(stat_id)
 
+func findsprite(stat_id):
+	
+	# I'm not just putting speechsprite up here, because
+	# of the fact that stat_id == 0 means nothing should be showing.
+	if stat_id == 1:
+		speechSprite.transparency = 0
+		foodSprite.transparency = 0
+	elif stat_id == 2:
+		speechSprite.transparency = 0
+		shelterSprite.transparency = 0
+	elif stat_id == 3:
+		speechSprite.transparency = 0
+		comfortSprite.transparency = 0
+	elif stat_id == 4:
+		speechSprite.transparency = 0
+		communitySprite.transparency = 0
+
+func startspritetimer(stat_id):
+	if stat_id == 1:	
+		foodEmoteTimer = 5
+		
+	elif stat_id == 2:
+		shelterEmoteTimer = 5
+		
+	elif stat_id == 3:
+		comfortEmoteTimer = 5
+		
+	elif stat_id == 4:
+		communityEmoteTimer = 5
+
+func updatespritetimer(subtract):
+
+##  0-3 - Check whether the need has to be fucked with or not
+func checkneed(stat_id: int): 
+	
+	# The prevFood stuff is to make sure that checkPercentage doesn't get 
+	# called every frame, because this func has to be updated!
+	# Basically, it tells it if there's no change, don't fuck with the timers.
 	if stat_id == 0:	
 		if prevFood != food:
 			checkpercentage(food, stat_id)
@@ -90,7 +150,7 @@ func countdowntimer(stat_id: int, subtract: float):
 		
 		if foodTimer <= 0:
 			if foodTimer > -60:
-				#Emote
+				emote(1)
 				checkneed(0)
 		
 	elif stat_id == 1:
@@ -98,7 +158,7 @@ func countdowntimer(stat_id: int, subtract: float):
 		
 		if shelterTimer <= 0:
 			if shelterTimer > -60:
-				#Emote
+				emote(2)
 				checkneed(1)
 		
 	elif stat_id == 2:
@@ -106,7 +166,7 @@ func countdowntimer(stat_id: int, subtract: float):
 		
 		if comfortTimer <= 0:
 			if comfortTimer > -60:
-				#Emote
+				emote(3)
 				checkneed(2)
 		
 	else:
@@ -114,5 +174,5 @@ func countdowntimer(stat_id: int, subtract: float):
 		
 		if communityTimer <= 0:
 			if communityTimer > -60:
-				#Emote
+				emote(4)
 				checkneed(3)
